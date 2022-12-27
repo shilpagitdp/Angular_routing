87890f23c282d9699a1592207798510905c4a0ae
shilpagitdp <shilpadp2000@gmail.com>
2022-12-27 19:00:31 +0530

    commit

---
+++ router/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+    //{provide: LocationStrategy, useClass: PathLocationStrategy}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
